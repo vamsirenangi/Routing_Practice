2177d9b2d298457fa99dfad3dcea75ea7e6ffad1
vamsirenangi <vamsirenangi1708@gmail.com>
2023-05-20 06:06:16 +0000

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import Header from './components/Header'
+import About from './components/About'
+import Home from './components/Home'
+import Contact from './components/Contact'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <BrowserRouter>
+    <div className="app-container">
+      <div className="responsive-container">
+        <Header />
+        <div className="app-body">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </div>
+    </div>
+  </BrowserRouter>
+)
+
+export default App
